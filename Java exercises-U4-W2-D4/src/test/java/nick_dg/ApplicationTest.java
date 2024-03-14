9ac5699b428a2e0ddd08a316169c3358a03aea37
Nick-DiGiacinto <151628711+Nick-DiGiacinto@users.noreply.github.com>
2024-03-14 17:39:30 +0100

    Add files via upload

---
+++ Java exercises-U4-W2-D4/src/test/java/nick_dg/ApplicationTest.java
@@ -0,0 +1,13 @@
+package nick_dg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
